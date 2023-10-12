0defebfe27ea1ef9980abeb6b2018ee13dc2bb57
sourabchoudhary <csourab888@gmail.com>
2023-10-13 00:57:38 +0530

    update Home.jsx

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import IntroCarRental from '../../components/introCarRental/introCarRental'
-import BookCar from '../../components/bookCar.jsx/BookCar'
+import BookCar from '../../components/bookCar/BookCar'
 import TripPlan from '../../components/tripPlan/TripPlan'
 import VehicleModels from '../../components/vehicleModels/VehicleModels'
 
